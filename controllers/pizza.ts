36f47319a586771add2bc02ec4bf6827e5cef2f3
amiguel-intermediait <amiguel@intermediait.com>
2022-06-23 13:52:46 -0300

    changes

--- controllers/pizza.ts
+++ controllers/pizza.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express"
-import { hasAllegensService, hasFoodTypeService, removefoodTypesService } from "../services/pizzaService"
+import { hasAllegensService, hasFoodTypeService, removeAllergensService, removefoodTypesService } from "../services/pizzaService"
 import {  arrayStringInterface, foodtypeInterface, ingredientInterface, ingredientTypeInterface, ingredientAllergent } from "../interfaces/interfaces";
 const Recipe = require("../models/foodtype") ;
 const Ingredient = require("../models/ingredient");
 const Foodtype = require("../models/recipe");
 export const hasAllergens = async(req: Request, res: Response) => {
     try {
-        const { allergens, recipe } = req.body;
-        return hasAllegensService(allergens,recipe);
+        const { allergens, recipe } = req.query;
+        const result = hasAllegensService(allergens,recipe);
+        res.json({
+            result
+        })
     } catch (error) {
         console.log(error);
         res.status(404).json({
@@ -19,7 +22,10 @@ export const hasAllergens = async(req: Request, res: Response) => {
 export const hasFoodTypes = async(req: Request, res: Response) => {
     try {
         const { foodtype, recipe } = req.body;
-        return hasFoodTypeService(foodtype,recipe);
+        const result = hasFoodTypeService(foodtype,recipe);
+        res.json({
+           result
+        })
     } catch (error) {
         console.log(error);
         res.status(404).json({
@@ -31,7 +37,10 @@ export const hasFoodTypes = async(req: Request, res: Response) => {
 export const removefoodTypes = async(req: Request, res: Response) => {
     try {
         const { foodtype, recipe } = req.body;
-        return removefoodTypesService(foodtype,recipe);
+        const result =  removefoodTypesService(foodtype,recipe);
+        res.json({
+            result
+        })
     } catch (error) {
         console.log(error);
         res.status(404).json({
@@ -43,34 +52,9 @@ export const removefoodTypes = async(req: Request, res: Response) => {
 export const removeAllergens = async(req: Request, res: Response) => {
     try {
         const { allergens, recipe } = req.body;
-        const { ingredients } = await Recipe.findOne({where: { name: recipe  },
-            include: [
-              {
-                model: Ingredient,
-                include: [
-                    Foodtype
-                ],
-              }
-            ],
-        });
-        const ingredientsArray: ingredientAllergent[] = ingredients.map((ingredient: ingredientInterface) => ({
-            name: ingredient.name,
-            allergen: ingredient.foodtype.isAllergen,
-            }
-        ));
-        const foodtypeArray: arrayStringInterface[] = allergens.map((allergen: string) => ({
-            name:allergen
-            }
-        ));
-        let responseArray : ingredientAllergent[] = [];
-        for(let i = 0; i < ingredientsArray.length; i++){
-            for(let j = 0; j < foodtypeArray.length; j++){
-                if(!((ingredientsArray[i].allergen)&&(ingredientsArray[i].name == foodtypeArray[j].name))){
-                    responseArray.push(ingredientsArray[i])
-                }
-            }
-        }
-        return res.json(responseArray);
+        const result =  removeAllergensService(allergens,recipe);
+        res.json({
+        })
     } catch (error) {
         console.log(error);
         res.status(404).json({
